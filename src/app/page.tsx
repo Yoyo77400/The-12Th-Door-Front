795a054dc47c62eb6da02f3d38fc31292509009e
AdrienDesclaux <adrien.desclaux@outlook.fr>
2025-07-11 22:30:25 +0200

    feature\ added card design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,11 @@
 "use client";
 import Image from "next/image";
+import { Card } from "@/components/ui/card";
 
 export default function Home() {
-  return <></>;
+  return (
+    <div className="container h-screen">
+      <Card className="w-[200px] h-[200px]" />
+    </div>
+  );
 }
