2e6d3e877101438c55bd29b0bdcbf8035fb19d44
Guillaume ALAMÉDA <guillaume@MacBook-Air-de-Guillaume.local>
2025-07-12 22:44:55 +0200

    Begin of the reward page

--- src/app/reward/page.tsx
+++ src/app/reward/page.tsx
@@ -2,7 +2,7 @@
 
 import Image from "next/image";
 import Cubes from "@/components/Cubes";
-import DynamicSection from "@/components/RewardSection";
+import RewardSection from "@/components/RewardSection";
 
 
 
@@ -10,7 +10,7 @@ import DynamicSection from "@/components/RewardSection";
 export default function Reward() {
   return (
 
-    <div className="flex flex-col h-[calc(100vh-3rem)] pt-8 px-4 sm:px-8 md:px-16 lg:px-24">
+    <div className="flex flex-col h-[calc(100vh-5rem)] pt-8 px-4 sm:px-8 md:px-16 lg:px-24">
 
     {/*Title*/}
     <div className="text-5xl font-bold mb-8 pt-6 text-white">Rewards</div>
@@ -20,7 +20,7 @@ export default function Reward() {
         <Cubes />
     
       {/* Dynamic section */}
-      <DynamicSection />
+      <RewardSection />
 
 
     </div>
