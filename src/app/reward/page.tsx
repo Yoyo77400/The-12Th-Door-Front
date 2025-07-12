be903076e7d32fd6a591ab719c733c307b2b86b9
Guillaume ALAMÉDA <guillaume@MacBook-Air-de-Guillaume.local>
2025-07-12 22:33:37 +0200

    Begin of the reward page

--- src/app/reward/page.tsx
+++ src/app/reward/page.tsx
@@ -2,26 +2,26 @@
 
 import Image from "next/image";
 import Cubes from "@/components/Cubes";
+import DynamicSection from "@/components/RewardSection";
 
 
 
-export default function Home() {
+
+export default function Reward() {
   return (
-    <div className="flex flex-col min-h-screen pt-8">
+
+    <div className="flex flex-col h-[calc(100vh-3rem)] pt-8 px-4 sm:px-8 md:px-16 lg:px-24">
 
     {/*Title*/}
-    <div className="text-5xl font-bold mb-8 p-6 text-white">Rewards</div>
+    <div className="text-5xl font-bold mb-8 pt-6 text-white">Rewards</div>
 
 
       {/* Section cubes */}
         <Cubes />
     
+      {/* Dynamic section */}
+      <DynamicSection />
 
-
-      {/* Contenu dynamique (à venir) */}
-      <main className="flex-1 p-4">
-        <p>Contenu dynamique à venir ici...</p>
-      </main>
 
     </div>
   );
---
+++ src/components/RewardSection.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+export default function DynamicSection() {
+  return (
+    <section className="flex-1 bg-white rounded-2xl h-1/2 p-6 shadow-md w-full text-gray-900 mb-4 mt-[1rem]">
+      <h2 className="text-lg font-semibold mb-2">Titre de la section</h2>
+      <p className="text-sm">Contenu dynamique à venir ici...</p>
+    </section>
+  );
+}
